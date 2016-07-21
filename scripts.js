6ad5e3debe6e7121926d299f509252d585ff133a
ANTHONY ORTIZ <anthony.ortiz@jpmchase.com>
2016-07-21 12:29:48 -0400

    points at localhost

---
+++ scripts.js
@@ -0,0 +1,23 @@
+﻿function onReady()
+   {
+   //alert('Hello world');
+   }
+
+var serviceUrl = 'http://localhost:63791/api/test';
+
+function sendRequest()
+{
+   var method = $('#method').val();
+
+   $.ajax({
+      dataType: "text",
+      type: method,
+      url: serviceUrl
+   }).done(function (data)
+   {
+      $('#value1').text(data);
+   }).error(function (jqXHR, textStatus, errorThrown)
+   {
+      $('#value1').text(jqXHR.responseText + textStatus);
+   });
+}
